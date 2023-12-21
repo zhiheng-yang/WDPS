974cb87f9e9626f7a456f935a74cf2c9e5000cf6
zhiheng-yang <young-z-h@outlook.com>
2023-12-21 23:10:37 +0100

    goal

--- linking/utilities/linking_loader.py
+++ linking/utilities/linking_loader.py
@@ -1,10 +1,13 @@
 from linking.inference.processor import Refined
-from linking.utilities.entities_format_print import print_format_wiki
+from linking.utilities.entities_format_print import print_format_wiki, format_wiki, fromat_wiki_list, goal_finder
 
 
-def linking(text):
+def linking(text, print=False):
     refined = Refined.from_pretrained(model_name='wikipedia_model_with_numbers',
                                       entity_set="wikipedia")
     spans = refined.process_text(text)
-    print_format_wiki(spans)
-    return spans
+    # formatted_wiki = format_wiki(spans)
+    formatted_wiki = fromat_wiki_list(spans)
+    if print:
+        print_format_wiki(spans)
+    return formatted_wiki
