43484f174143a685dc56a2c7424ca1d1fdc16ead
guozhinuan <guozhinuan@bigo.sg>
2023-12-22 10:38:43 +0000

    complict

--- answer/fact_checking/fact_checking.py
+++ answer/fact_checking/fact_checking.py
@@ -2,10 +2,11 @@ from transformers import RobertaTokenizer, RobertaForSequenceClassification
 import torch
 
 # 0 for supports and 1 for refutes
-def fact_checking(_claim, _evidence):
+def fact_checking(_claim, _evidence, local=True):
 
-    tokenizer = RobertaTokenizer.from_pretrained('Dzeniks/roberta-fact-check')
-    model = RobertaForSequenceClassification.from_pretrained('Dzeniks/roberta-fact-check')
+    model_name = "JLei/climate_fever_roberta-base-fact-checking" if local else 'Dzeniks/roberta-fact-check'
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    model = RobertaForSequenceClassification.from_pretrained(model_name)
 
     x = tokenizer.encode_plus(_claim, _evidence, return_tensors="pt")
 
